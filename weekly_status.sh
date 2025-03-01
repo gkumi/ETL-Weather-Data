adfd3a6f95148c1b51ed0c6b7ba2c3ecc9e29aaf
gkumi <gkumi94@gmail.com>
2025-03-01 17:23:38 -0500

    Update weekly_status.sh

--- weekly_status.sh
+++ weekly_status.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo $(tail -7 synthetic_historical_fc_accuracy.tsv  | cut -f6) > scratch.txt
 week_fc=($(echo $(cat scratch.txt)))
+
 # validate result:
 for i in {0..6}; do
     echo ${week_fc[$i]}
@@ -12,6 +13,7 @@ for i in {0..6}; do
   then
     week_fc[$i]=$(((-1)*week_fc[$i]))
   fi
+  
   # validate result:
   echo ${week_fc[$i]}
 done
